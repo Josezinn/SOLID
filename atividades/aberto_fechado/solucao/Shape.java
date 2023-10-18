6f4285501f58803f165103506969b4779e0ec88a
José Francisco <89284667+Josezinn@users.noreply.github.com>
2023-10-18 00:30:27 -0300

    Solução Atividade Aberto Fechado

---
+++ atividades/aberto_fechado/solucao/Shape.java
@@ -0,0 +1,6 @@
+//Solucionando o problema do principío OCP, fechando a classe para modificações.
+//Qualquer tipo de forma adicionada, bastará criar uma classe extendendo a Shape.
+
+public abstract class Shape {
+    public abstract void drow();
+}
---
+++ atividades/aberto_fechado/solucao/Triangle.java
@@ -0,0 +1,5 @@
+public class Triangle extends Shape{
+    public void drow() {
+        System.out.println("Drawing a circle");
+    }
+}
